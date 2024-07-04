0f75186c7798b5706b20c3ec6278fa8f9eb0807f
alex <alex@alex-XPS-8700>
2024-07-04 12:10:39 -0600

    se agregó la funcionalidad de puntos.

--- src/nbackgame/HiloBack.java
+++ src/nbackgame/HiloBack.java
@@ -14,13 +14,11 @@ import javax.swing.JLabel;
  * @author Arleth
  *
  */
-
 public class HiloBack extends Thread {
 
     private List<Integer> listanum;
     private JLabel lbNum;
     private int numActual;
-    
 
     public HiloBack(JLabel lbNum) {
         this.lbNum = lbNum;
@@ -30,34 +28,38 @@ public class HiloBack extends Thread {
     public int getNumActual() {
         return numActual;
     }
-    
 
     public void setLbNum(JLabel lbNum) {
         this.lbNum = lbNum;
     }
 
-    public int getNBack (int n){
-        
-       return listanum.get(listanum.size()-n);
+    public int getNBack(int n) {
+        if (listanum.size() >= (n + 1)) {
+            return listanum.get(listanum.size() - (n + 1));
+        } else {
+            return 0;
+        }
     }
-    
+
     @Override
     public void run() {
-        
-        
+
         while (true) {
             try {
                 numActual = new Random().nextInt(0, 9);
                 listanum.add(numActual);
                 lbNum.setText("");
-                sleep(1000);
+//                sleep(1000);
                 lbNum.setText(String.valueOf(numActual));
+                System.out.println("Actual (" + numActual + ")");
+                System.out.println("N anterior (" + getNBack(2) + ")");
                 sleep(5000);
             } catch (InterruptedException ex) {
+                System.err.println("Error: " + ex.getMessage());
 
             }
         }
-        
+
     }
 
 }
--- src/nbackgame/PanelNBack.java
+++ src/nbackgame/PanelNBack.java
@@ -9,14 +9,19 @@ package nbackgame;
  * @author Arleth
  */
 public class PanelNBack extends javax.swing.JPanel {
-
+    private int n;
+    private int puntos;
+    private HiloBack hilo;
     /**
      * Creates new form PanelNBack
      */
     public PanelNBack() {
+        n=2;
+        puntos=5;
         initComponents();
-        HiloBack hilo = new HiloBack(lbNumeros);
+        hilo = new HiloBack(lbNumeros);
         hilo.start();
+        actualizarPuntos();
     }
     
 
@@ -29,11 +34,13 @@ public class PanelNBack extends javax.swing.JPanel {
         lbNumeros = new javax.swing.JLabel();
         btnDiferente = new javax.swing.JButton();
         btnIgual = new javax.swing.JButton();
+        lblPuntos = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         jPasswordField1.setText("jPasswordField1");
 
         lbNumeros.setFont(new java.awt.Font("Tahoma", 3, 24)); // NOI18N
-        lbNumeros.setBorder(javax.swing.BorderFactory.createLineBorder(null));
+        lbNumeros.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         btnDiferente.setText("Diferente");
         btnDiferente.addActionListener(new java.awt.event.ActionListener() {
@@ -49,6 +56,10 @@ public class PanelNBack extends javax.swing.JPanel {
             }
         });
 
+        lblPuntos.setText("jLabel1");
+
+        jLabel1.setText("Puntos:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
@@ -60,16 +71,27 @@ public class PanelNBack extends javax.swing.JPanel {
                 .addComponent(btnIgual)
                 .addGap(82, 82, 82))
             .addGroup(layout.createSequentialGroup()
-                .addGap(132, 132, 132)
-                .addComponent(lbNumeros, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(lblPuntos))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(lbNumeros, javax.swing.GroupLayout.PREFERRED_SIZE, 57, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(62, Short.MAX_VALUE)
-                .addComponent(lbNumeros, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(40, 40, 40)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblPuntos)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addComponent(lbNumeros, javax.swing.GroupLayout.PREFERRED_SIZE, 65, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnDiferente)
                     .addComponent(btnIgual))
@@ -78,19 +100,35 @@ public class PanelNBack extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnDiferenteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDiferenteActionPerformed
-              
+        if (hilo.getNumActual() != hilo.getNBack(n)) {
+            puntos ++;
+        }else{
+            puntos --;
+        }
+        actualizarPuntos();
     }//GEN-LAST:event_btnDiferenteActionPerformed
 
     private void btnIgualActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIgualActionPerformed
-        
+         if (hilo.getNumActual() == hilo.getNBack(n)) {
+            puntos ++;
+        }else{
+            puntos --;
+        }
+        actualizarPuntos();
         
     }//GEN-LAST:event_btnIgualActionPerformed
 
+    
+    private void actualizarPuntos(){
+        lblPuntos.setText(String.valueOf(puntos));
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnDiferente;
     private javax.swing.JButton btnIgual;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JLabel lbNumeros;
+    private javax.swing.JLabel lblPuntos;
     // End of variables declaration//GEN-END:variables
 }
